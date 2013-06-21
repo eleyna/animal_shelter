352010225011db32a2010bad9897e4845ce73d63
eleyna <eleyna@gmail.com>
2013-06-20 22:12:25 -0400

    added functionality to give up an animal

--- animal_shelter.rb
+++ animal_shelter.rb
@@ -26,17 +26,29 @@ class Shelter
       puts "\n"
       puts "Available for adoption:"
       animals.each {|animal| puts animal.name if animal.available_to_adopt == true}
+      clients.each {|client| client.animals.each {|animal| puts animal.name if animal.available_to_adopt == true}}
+    end
+
+    def intake_new_animal(animal)
+      animals << animal
     end
 end
 
 class Client
-  attr_accessor :name, :age, :gender, :kids, :num_pets
-  def initialize(name, age, gender, kids, num_pets)
+  attr_accessor :name, :age, :gender, :kids, :num_pets, :animals
+  def initialize(name, age, gender, kids, num_pets, animals)
     @name = name
     @age = age
     @gender = gender
     @kids = kids
     @num_pets = num_pets
+    @animals = animals
+  end
+
+  def give_up_animal(animal, shelter)
+    animals.delete(animal)
+    shelter.intake_new_animal(animal)
+    @num_pets -= 1
   end
 end
 
@@ -52,19 +64,51 @@ class Animal
   end
 end
 
-**********__________MAIN__________**********
 
-The_Animal_Shelter = Shelter.new()
-The_Animal_Shelter.clients << Client.new("Joe", 30, "male", 2, 5)
-The_Animal_Shelter.clients << Client.new("Jeff", 35, "male", 3, 7)
-The_Animal_Shelter.clients << Client.new("Sarah", 25, "female", 0, 1)
-The_Animal_Shelter.animals << Animal.new("Milo", "cat", 5, "male", "string", true)
-The_Animal_Shelter.animals << Animal.new("Scooby", "dog", 10, "male", "bone", true)
-The_Animal_Shelter.animals << Animal.new("Sparky", "dog", 2, "male", "cat", false)
+############__________MAIN__________############
 
-The_Animal_Shelter.print_animal_list()
-The_Animal_Shelter.print_client_list()
-The_Animal_Shelter.print_adoption_list()
+animal_haven = Shelter.new()
+
+felix_the_cat = Animal.new("Felix", "cat", 2, "male", "string", true)
+sasha_the_dog = Animal.new("Sasha", "dog", 10, "female", "bone", true)
+milo_the_cat = Animal.new("Milo", "cat", 5, "male", "string", true)
+scooby_the_dog = Animal.new("Scooby", "dog", 10, "male", "bone", true)
+sparky_the_dog = Animal.new("Sparky", "dog", 2, "male", "cat", false)
+
+joe = Client.new("Joe", 30, "male", 2, 2, [felix_the_cat, sasha_the_dog])
+jeff = Client.new("Jeff", 35, "male", 3, 0, [])
+sarah = Client.new("Sarah", 25, "female", 0, 0, [])
+
+animal_haven.clients << joe
+animal_haven.clients << jeff
+animal_haven.clients << sarah
+
+animal_haven.animals << milo_the_cat
+animal_haven.animals << scooby_the_dog
+animal_haven.animals << sparky_the_dog
+
+puts "\n*****************************************"
+animal_haven.print_adoption_list()
+puts "\n*****************************************"
+puts "\nAnimals in shelter:"
+animal_haven.print_animal_list()
+puts "\n*****************************************"
+puts "\nClient list:"
+animal_haven.print_client_list()
+puts ""
+
+joe.give_up_animal(felix_the_cat, animal_haven)
+
+puts "\n*****************************************"
+animal_haven.print_adoption_list()
+puts "\n*****************************************"
+puts "\nAnimals in shelter:"
+animal_haven.print_animal_list()
+puts "\n*****************************************"
+puts "\nClient list:"
+animal_haven.print_client_list()
+puts ""
+
 
 
 
